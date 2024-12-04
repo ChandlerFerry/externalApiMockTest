8c07ca02b8bf0c8dff5be2abe917dd963c3edaea
Chandler Ferry <chandlerferry@gmail.com>
2024-12-04 04:00:21 -0800

    chore: nock external api mock

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import * as nock from 'nock';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -13,6 +14,15 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    nock('https://jsonplaceholder.typicode.com')
+    .get('/todos/1')
+    .reply(200, {
+      userId: 2,
+      id: 2,
+      title: 'mocked title',
+      completed: true,
+    });
   });
 
   it('/ (GET)', () => {
@@ -27,10 +37,10 @@ describe('AppController (e2e)', () => {
       .get('/api/getData')
       .expect(200)
       .expect({
-        userId: 1,
-        id: 1,
-        title: 'delectus aut autem',
-        completed: false,
+        userId: 2,
+        id: 2,
+        title: 'mocked title',
+        completed: true,
       });
   });
 });
