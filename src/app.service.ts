ea44b9b66018384af14122439fa49c1606ef4b96
Chandler Ferry <chandlerferry@gmail.com>
2024-12-05 12:41:04 -0800

    chore: almost envoy

--- src/app.service.ts
+++ src/app.service.ts
@@ -10,16 +10,29 @@ export class AppService {
     "title": string,
     "completed": boolean
   }> {
-    // Load the environment variables from /etc/config/some_partner_1.yaml
-    const file = fs.readFileSync('/etc/config/some_partner_1.yaml', 'utf8');
-
-    const config = jsYaml.load(file) as {
-      EXTERNAL_API: string;
-    };
-
-    if (!config.EXTERNAL_API) {
-      throw new Error('EXTERNAL_API not found in some_partner_1.yaml');
+    let config = {
+      EXTERNAL_API: ''
     }
+
+    if(process.env.ENVOYENABLED === "enabled") {
+      config.EXTERNAL_API = 'https://jsonplaceholder.typicode.com/todos/1';
+    } else {
+      // Load the environment variables from /etc/config/some_partner_1.yaml
+      const file = fs.readFileSync('/etc/config/some_partner_1.yaml', 'utf8');
+
+      config = jsYaml.load(file) as {
+        EXTERNAL_API: string;
+      };
+
+      if (!config.EXTERNAL_API) {
+        throw new Error('EXTERNAL_API not found in some_partner_1.yaml');
+      }
+    }
+
+    if(!config.EXTERNAL_API) {
+      throw new Error('EXTERNAL_API not found');
+    }
+
 
     const response = await axios.get(config.EXTERNAL_API);
     return response.data;
